3bc00385e4bffaa7d682627982b752fde7578b84
Azure2212 <Dasanbob22122002@gmail.com>
2024-05-03 00:39:31 +0700

    updated resnetCbam

--- resnet_model/train_cbam.py
+++ resnet_model/train_cbam.py
@@ -80,6 +80,8 @@ parser.add_argument('--early-stopping', default= all_inital_argument['early-stop
 parser.add_argument('--lr-decrease', default= all_inital_argument['lr-decrease'], type=int, help='value learning rate decrease after 5 epochs not better')
 args, unknown = parser.parse_known_args()
 configs = vars(args)
+args["gpus"] = eval(args["gpus"])
+print("parameters: ")
 for key in args.keys(): print("+ %s: %s"%(key,args[key]))
 
 
